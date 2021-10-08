41ebb65078bbb93453c76a8950b361c5ecb3012d
dqhuy78 <dqhuy78@gmail.com>
2021-10-08 09:07:31 +0700

    feat: remove unfinish event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import Noel from './components/Noel';
-import NewYear from './components/NewYear';
 
 const App = () => {
 
     return (
         <div className="App">
-            <NewYear />
+            <Noel />
         </div>
     );
 }
